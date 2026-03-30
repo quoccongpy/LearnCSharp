18d03865c568ed7d71584a11bce72f40551df90e
quoccongpy <75265778+quoccongpy@users.noreply.github.com>
2026-03-31 02:07:16 +0700

    Fix api product (#10)

--- LearnCSharp.Application/Models/DTOs/Product/ProductUpdateDTO.cs
+++ LearnCSharp.Application/Models/DTOs/Product/ProductUpdateDTO.cs
@@ -6,11 +6,11 @@ namespace LearnCSharp.Application.Models.DTOs.Product
     {
         public string? Name { get; set; }
         public float? Price { get; set; }
-        public IFormFile? Thumbnaill { get; set; }
+        public IFormFile? Thumbnail { get; set; }
         public string? Description { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public int? CategoryId { get; set; }
-        public IList<IFormFile>? Image { get; set; }
+        public IList<IFormFile>? Images { get; set; }
         public List<int>? ListRetainIdsImage { get; set; }
     }
 }
