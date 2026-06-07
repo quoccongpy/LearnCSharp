606df8d07baf12ae55cba865025516e150c260c1
quoccongpy <phamquoccongpy0703@gmail.com>
2026-06-07 22:07:00 +0700

    Update Field Two Table OrderDetails and Orders

--- LearnCSharp.Infrastructure/Persistence/Configurations/OrderDetailsConfiguration.cs
+++ LearnCSharp.Infrastructure/Persistence/Configurations/OrderDetailsConfiguration.cs
@@ -13,10 +13,14 @@ namespace LearnCSharp.Infrastructure.Persistence.Configurations
             builder.Property(a => a.Id).HasColumnName("id");
             builder.Property(a => a.OrderId).HasColumnName("order_id");
             builder.Property(a => a.ProductId).HasColumnName("product_id");
-            builder.Property(a => a.Price).HasColumnName("price");
             builder.Property(a => a.Quantity).HasColumnName("quantity");
             builder.Property(a => a.Total).HasColumnName("total");
             builder.Property(a => a.ProductVariantId).HasColumnName("product_variant_id").IsRequired(false);
+            builder.Property(a => a.ProductName).HasColumnName("product_name").HasMaxLength(100).IsRequired();
+            builder.Property(a => a.SizeName).HasColumnName("size_name").HasMaxLength(100).IsRequired(false);
+            builder.Property(a => a.CrustName).HasColumnName("crust_name").HasMaxLength(100).IsRequired(false);
+            builder.Property(a => a.Note).HasColumnName("note").HasMaxLength(250).IsRequired(false);
+            builder.Property(a => a.UnitPrice).HasColumnName("unit_price").IsRequired();
             //builder.Property(a => a.Color).HasColumnName("color").HasMaxLength(20);
         }
     }
