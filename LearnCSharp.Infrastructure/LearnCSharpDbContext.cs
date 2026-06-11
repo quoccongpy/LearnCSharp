53308006c940f933d573b10ae91bd8faa52e338b
quoccongpy <phamquoccongpy0703@gmail.com>
2026-06-11 14:23:07 +0700

    Add Cart and CartItem Table

--- LearnCSharp.Infrastructure/LearnCSharpDbContext.cs
+++ LearnCSharp.Infrastructure/LearnCSharpDbContext.cs
@@ -22,6 +22,8 @@ namespace LearnCSharp.Infrastructure
         public DbSet<Crust> Crust { get; set; }
         public DbSet<Size> Size { get; set; }
         public DbSet<ProductVariant> ProductVariant { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<CartItem> CartItem { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -44,6 +46,8 @@ namespace LearnCSharp.Infrastructure
             builder.ApplyConfiguration(new SizeConfiguration());
             builder.ApplyConfiguration(new CrustConfiguration());
             builder.ApplyConfiguration(new ProductVariantConfiguration());
+            builder.ApplyConfiguration(new CartConfiguration());
+            builder.ApplyConfiguration(new CartItemConfiguration());
 
             base.OnModelCreating(builder);
         }
---
+++ LearnCSharp.Infrastructure/Persistence/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using LearnCSharp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearnCSharp.Infrastructure.Persistence.Configurations
+{
+    public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+    {
+        public void Configure(EntityTypeBuilder<CartItem> builder)
+        {
+            builder.ToTable("cart_item");
+            builder.HasKey(ci => ci.Id);
+            builder.Property(ci => ci.Id).HasColumnName("id");
+            builder.Property(ci => ci.CartId).HasColumnName("cart_id");
+            builder.Property(ci => ci.ProductId).HasColumnName("product_id");
+            builder.Property(ci => ci.ProductVariantId).HasColumnName("product_variant_id").IsRequired(false);
+            builder.Property(ci => ci.Quantity).HasColumnName("quantity");
+            builder.Property(ci => ci.Note).HasColumnName("note").HasMaxLength(72);
+            builder.HasOne(ci => ci.Cart)
+                .WithMany(c => c.CartItems)
+                .HasForeignKey(ci => ci.CartId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(ci => ci.Product)
+                .WithMany()
+                .HasForeignKey(ci => ci.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(ci => ci.ProductVariant)
+                .WithMany()
+                .HasForeignKey(ci => ci.ProductVariantId)
+                .OnDelete(DeleteBehavior.Restrict);
+            builder.HasIndex(ci => new { ci.CartId, ci.ProductId, ci.ProductVariantId, ci.Note }).IsUnique();
+        }
+    }
+}
